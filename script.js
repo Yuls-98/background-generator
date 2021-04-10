28a7480fccaef155e62c5f3bcbb4910d78d85ab5
Julio <72820369+Yuls-98@users.noreply.github.com>
2021-04-10 17:56:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-function changeBackground(){
+function changeGradient(){
     body.style.background = "linear-gradient(to right, "
     +color1.value+", "+color2.value+" )";
 }
 
-color1.addEventListener("input", changeBackground);
-color2.addEventListener("input", changeBackground);
+color1.addEventListener("input", changeGradient);
+color2.addEventListener("input", changeGradient);
