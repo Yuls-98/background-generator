d467042baccc5b21c91461685776e24f2ae8545d
Julio <72820369+Yuls-98@users.noreply.github.com>
2021-04-10 19:00:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var listItems = document.querySelectorAll("li");
 
-function changeGradient(){
-    body.style.background = "linear-gradient(to right, "
-    +color1.value+", "+color2.value+" )";
-
-    css.textContent = body.style.background + ";";
+function inputLength() {
+	return input.value.length;
 }
 
-color1.addEventListener("input", changeGradient);
-color2.addEventListener("input", changeGradient);
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	li.addEventListener("click", checkItem);
+	ul.appendChild(li);
+	createListButton(li);
+	input.value = "";
+}
+
+
+function createListButton(li){
+	var button = document.createElement("button");
+	button.appendChild(document.createTextNode("Eliminar"));
+	li.appendChild(button);
+	button.classList.add("liBttn");
+	button.addEventListener("click", deleteItem);
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function checkItem(event){
+	event.target.classList.toggle("done");
+}
+
+function deleteItem(event){
+	event.target.parentElement.remove();
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
